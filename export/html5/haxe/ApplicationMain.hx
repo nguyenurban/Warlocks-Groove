c0a49e923bd0f546ffa547d117a334d12262d06e
Andrew Nguyen <nguyeand@uw.edu>
2022-05-24 02:43:51 -0700

    should maybe check music runs correctly one last time before launch?

--- export/html5/haxe/ApplicationMain.hx
+++ export/html5/haxe/ApplicationMain.hx
@@ -32,7 +32,7 @@ class ApplicationMain
 
 		ManifestResources.init(config);
 
-		app.meta["build"] = "104";
+		app.meta["build"] = "105";
 		app.meta["company"] = "HaxeFlixel";
 		app.meta["file"] = "Warlocks Groove";
 		app.meta["name"] = "Warlocks Groove";
