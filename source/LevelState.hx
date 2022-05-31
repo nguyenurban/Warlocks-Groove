621bf12452e0e4b3069df4bc8e544952f70fdac8
Andrew Nguyen <nguyeand@uw.edu>
2022-05-31 16:37:43 -0700

    first two bosses affected by hard mode

--- source/LevelState.hx
+++ source/LevelState.hx
@@ -762,11 +762,11 @@ class LevelState extends FlxState
 			switch (src)
 			{
 				case "From WaterStrider":
-					for (angle in 0...20)
+					for (angle in 0...(LevelStats.hard_mode ? 30 : 20))
 					{
-						var dir = FlxVelocity.velocityFromAngle(angle * 18, 50);
+						var dir = FlxVelocity.velocityFromAngle(angle * (LevelStats.hard_mode ? 12 : 18), 50);
 						var tar = e.getMidpoint().add(dir.x, dir.y);
-						_projectiles.add(new StriderShockwave(e.getMidpoint().x, e.getMidpoint().y, null, tar, src, 85));
+						_projectiles.add(new StriderShockwave(e.getMidpoint().x, e.getMidpoint().y, null, tar, src, (LevelStats.hard_mode ? 95 : 85)));
 					}
 				case "From Alligator":
 					if (cast(e, Alligator).isRapidFiring())
